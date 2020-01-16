db2fb5d5ad7a79c083b4144767ef9674dd75d069
Richard <barlatko@gmail.com>
2020-01-16 12:39:46 +0100

    Create ApiHelper.cs

---
+++ WebSupport.API/Helpers/ApiHelper.cs
@@ -0,0 +1,29 @@
+  
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebSupport.API.Controllers
+{
+
+  public static class ApiHelper
+  {
+  
+    public static HttpClient ApiClient {get;set;}
+    
+    private readonly string Api = "https://rest.websupport.sk";
+    
+    public static void InitializeClient()
+    {
+      ApiClient = new HttpClient();
+      ApiClient.BaseAddress = new Uri(Api);
+      ApiClient.DefaultRequestHeaders.Accept.Clear();
+      ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    }
+  }
+}
